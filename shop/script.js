b038a9b98efffe1d26485909ae4af42c5a185601
Tsum-EXE <117777715+Tsum-EXE@users.noreply.github.com>
2023-12-29 15:20:13 -0800

    Add files via upload

---
+++ shop/script.js
@@ -0,0 +1,63 @@
+const hidden1 = document.querySelectorAll('.hidden1');
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entryIter) => {
+        if (entryIter.isIntersecting) {
+            entryIter.target.classList.add('shown1');
+        }
+        else {
+            entryIter.target.classList.remove('shown1');
+        }
+
+    });
+});
+hidden1.forEach((element) =>
+    observer2.observe(element)
+);
+
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove2 = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+/* -- Had to add extra lines for touch events -- */
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove2(e);
+
+window.ontouchmove = e => handleOnMove2(e.touches[0]);
+
+
